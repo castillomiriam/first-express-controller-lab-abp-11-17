870e4652a40f75eaccd0be6da049faa05d94018c
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-29 03:12:28 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,13 +1,15 @@
 const SiteController = {};
-
- SiteController.Index(req, resp){
-     resp.render('site/index');
-   }
- SiteController.About(req, resp){
-     resp.render('site/about');
-   }
- SiteController.Contact(req, resp){
-     resp.render('site/contact');
-   }
-
+ 
+ SiteController.Index = function(req, resp) {
+   resp.render('site/index');
+ }
+ 
+ SiteController.About = function(req, resp) {
+   resp.render('site/about');
+ }
+ 
+ SiteController.Contact = function(req, resp) {
+   resp.render('site/contact');
+ }
+ 
  module.exports = SiteController;
