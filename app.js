39958b6540a0890fd9760e1d9b27833ed1f0d6e7
Miriam Castillo <castillo.m.mc@gmail.com>
2017-12-29 03:13:39 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-const SiteController = require(".controllers/SiteController.js");
+var SiteController = require(".controllers/SiteController.js");
 
   app.get('/', SiteController.Index);
   app.get('/about', SiteController.About);
